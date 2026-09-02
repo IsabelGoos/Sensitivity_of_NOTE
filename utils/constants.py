97dfefa5e55fe1d81524d775ab9dbf4b0213b321
IsabelGoos <isabel.astrid.goos.bosco@gmail.com>
2026-09-02 12:14:32 +0200

    Corrections

--- utils/constants.py
+++ utils/constants.py
@@ -3,46 +3,24 @@ import numpy as np
 # Basic settings
 DPI = 300
 
-# Atomic numbers (Z) and atomic weights (A)
+# Atomic numbers Z (first value) and atomic weights A (second value).
 # The A values are obtained by averaging over the possible isotopes 
-# and using the abundances given in the "NUCLEAR WALLET CARDS"
-# elements = {
-#     "H":  (1,   1.01), "He": (2,   4.00),
-#     "Li": (3,   6.92), "Be": (4,   9.00),
-#     "B":  (5,  10.80), "C":  (6,  12.01),
-#     "N":  (7,  14.01), "O":  (8,  16.00),
-#     "F":  (9,  19.00), "Ne": (10, 20.19),
-#     "Na": (11, 22.99), "Mg": (12, 24.31),
-#     "Al": (13, 26.98), "Si": (14, 28.09),
-#     "P":  (15, 30.97), "S":  (16, 32.07),
-#     "Cl": (17, 35.45), "Ar": (18, 39.95),
-#     "K":  (19, 39.10), "Ca": (20, 40.08),
-#     "Sc": (21, 45.00), "Ti": (22, 47.92),
-#     "V":  (23, 51.00), "Cr": (24, 52.00),
-#     "Mn": (25, 54.94), "Fe": (26, 55.85),
-#     "Co": (27, 59.00), "Ni": (28, 58.69),
-#     "Cu": (29, 63.62), "Zn": (30, 65.47),
-#     "Ga": (31, 69.80), "Ge": (32, 72.72),
-#     "As": (33, 75.00), "Se": (34, 79.04),
-#     "Br": (35, 79.90), "Kr": (36, 83.89),
-#     "Rb": (37, 85.56), "Sr": (38, 87.71), 
-# }
-
+# and using the abundances given in the "NUCLEAR WALLET CARDS".
 elements = {
-    "H":  (1,   1.008), "He": (2,   4.00),
+    "H":  (1,   1.01), "He": (2,   4.00),
     "Li": (3,   6.92), "Be": (4,   9.00),
     "B":  (5,  10.80), "C":  (6,  12.01),
-    "N":  (7,  14.01), "O":  (8,  15.999),
+    "N":  (7,  14.01), "O":  (8,  16.00),
     "F":  (9,  19.00), "Ne": (10, 20.19),
-    "Na": (11, 23.0), "Mg": (12, 24.305),
-    "Al": (13, 26.982), "Si": (14, 28.086),
-    "P":  (15, 30.97), "S":  (16, 32.065),
+    "Na": (11, 22.99), "Mg": (12, 24.31),
+    "Al": (13, 26.98), "Si": (14, 28.09),
+    "P":  (15, 30.97), "S":  (16, 32.07),
     "Cl": (17, 35.45), "Ar": (18, 39.95),
-    "K":  (19, 39.0), "Ca": (20, 40.078),
-    "Sc": (21, 45.00), "Ti": (22, 47.0),
-    "V":  (23, 51.00), "Cr": (23, 51.996),
-    "Mn": (25, 54.94), "Fe": (26, 55.845),
-    "Co": (27, 58.90), "Ni": (28, 58.693),
+    "K":  (19, 39.10), "Ca": (20, 40.08),
+    "Sc": (21, 45.00), "Ti": (22, 47.92),
+    "V":  (23, 51.00), "Cr": (24, 52.00),
+    "Mn": (25, 54.94), "Fe": (26, 55.85),
+    "Co": (27, 59.00), "Ni": (28, 58.69),
     "Cu": (29, 63.62), "Zn": (30, 65.47),
     "Ga": (31, 69.80), "Ge": (32, 72.72),
     "As": (33, 75.00), "Se": (34, 79.04),
@@ -50,7 +28,7 @@ elements = {
     "Rb": (37, 85.56), "Sr": (38, 87.71), 
 }
 
-# Elements present in the Earth composition
+# Elements present in the Earth (with abundances above 0.01%).
 labels_Earth = [
     "H", "C", "N", "O", "Na", "Mg", "Al", "Si", "P", "S", 
     "Cl", "Ar", "K", "Ca", "Cr", "Mn", "Fe", "Ni", "Br"
@@ -58,8 +36,15 @@ labels_Earth = [
 Z_Earth = np.array([elements[e][0] for e in labels_Earth])
 A_Earth = np.array([elements[e][1] for e in labels_Earth])
 
-# Elemental mass fractions for different materials
-# From 
+# Element fractions for different composites.
+# BE   = bulk Earth
+# BSE  = bulk silicate Earth
+# MORB = mid-ocean ridge basalt
+# BE, core, BSE from:
+# McDonough, W. F. (2014). 3.16–Compositional model for the Earth’s core.
+# Treatise on geochemistry, 2, 559-577.
+# MORB from:
+# Gale, A., et al. (2013). The chemical compositions of global MORB and their implications for mantle properties. In AGU Fall Meeting Abstracts (Vol. 2013, pp. V32A-01).
 wt_rocks = {
     "H":  {"BE":  0.2600, "core":  0.0600, "BSE":  0.1000, "MORB":  0.2000},
     "O":  {"BE": 30.0000, "core":  1.0000, "BSE": 44.0000, "MORB": 44.3800},
